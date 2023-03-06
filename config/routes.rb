e3f7d2c72569d725a11ffa716ce0345ba92668af
MohamedCK <abdulhakimm280@gmail.com>
2023-03-06 19:47:36 +0200

    - Update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_for :users, skip: [:sessions, :registrations],
+        controllers: {
+          sessions: 'api/v1/users/sessions',
+          registrations: 'api/v1/users/registrations',
+        }
+    end
+  end
   root "root#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
