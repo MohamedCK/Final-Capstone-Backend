d05d6508378ffd69a9bbdbb029267b1ffc5769c4
MohamedCK <abdulhakimm280@gmail.com>
2023-03-06 21:16:37 +0200

    - Add user sessions controller with json response

--- app/controllers/api/v1/users/sessions/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions/sessions_controller.rb
@@ -1,27 +1,74 @@
-# frozen_string_literal: true
+class Api::V1::Users::SessionsController < Devise::SessionsController
+  respond_to :json
 
-class Api::V1::Users::Sessions::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  # POST /api/v1/users/sign_in
+  def create
+    user = User.find_for_database_authentication(email: params[:user][:email])
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+    if user && user.valid_password?(params[:user][:password])
+      sign_in(user)
+      render json: { message: 'Logged in successfully.', data: user }
+    else
+      render json: { message: 'Invalid email or password.' }, status: :unauthorized
+    end
+  end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  # DELETE /api/v1/users/sign_out
+  def destroy
+    if current_user
+      sign_out(current_user)
+      render json: { message: 'Logged out successfully.' }
+    else
+      render json: { message: 'You need to sign in or sign up before continuing.' }, status: :unprocessable_entity
+    end
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  private
 
-  # protected
+  def respond_with(_resource, _opts = {})
+    current_user ? log_in_success : log_in_failure
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def respond_to_on_destroy
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split.last,
+                               ENV.fetch('DEVISE_JWT_SECRET_KEY')).first
+
+      current_user = User.find(jwt_payload['sub'])
+
+      current_user ? log_out_success : log_out_failure
+    else
+      log_out_failure
+    end
+  end
+
+  def log_in_success
+    render json: {
+      status: 200,
+      message: 'Logged in sucessfully.',
+      data: UserSerializer.new(current_user)
+    }, status: :ok
+  end
+
+  def log_in_failure
+    render json: {
+      status: 401,
+      message: "Logged in failure. #{resource.errors.full_messages.to_sentence}",
+      data: UserSerializer.new(current_user)
+    }, status: :unauthorized
+  end
+
+  def log_out_success
+    render json: {
+      status: 200,
+      message: 'Logged out sucessfully.'
+    }, status: :ok
+  end
+
+  def log_out_failure
+    render json: {
+      status: 401,
+      message: 'Logged out failure.'
+    }, status: :unauthorized
+  end
 end
