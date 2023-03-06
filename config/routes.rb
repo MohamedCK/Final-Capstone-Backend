fc25491e33fba863f1f963eddc8b4b52196106f2
MohamedCK <abdulhakimm280@gmail.com>
2023-03-06 21:24:08 +0200

    - Update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      devise_for :users, skip: [:sessions, :registrations],
-        controllers: {
-          sessions: 'api/v1/users/sessions',
-          registrations: 'api/v1/users/registrations',
-        }
+      devise_for :users do 
+        resources :registration, only: :create, controller: 'api/v1/users/registration' do 
+          post :cancel, action: :destroy, on: :collection
+        end
+        resources :sessions, only: [:create, :destroy], controller: 'api/v1/users/sessions'
     end
   end
-  root "root#index"
+end
+root "root#index"
 end
