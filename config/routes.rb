9079a8f2640d7cb5cf0330eaed221676f0596e62
MohamedCK <abdulhakimm280@gmail.com>
2023-03-10 14:23:24 +0200

    - Setup the routes for the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,27 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      devise_for :users do 
-        resources :registration, only: :create, controller: 'api/v1/users/registration' do 
-          post :cancel, action: :destroy, on: :collection
-        end
-        resources :sessions, only: [:create, :destroy], controller: 'api/v1/users/sessions'
+    # ...
+    mount Rswag::Api::Engine => '/api-docs'
+    mount Rswag::Ui::Engine => '/api-docs'
+    # ...
+  scope :api, defaults: { format: :json } do
+    scope :v1 do
+      devise_for :users,
+        controllers: {
+          registration: 'api/v1/users/registrations',
+          sessions: 'api/v1/users/sessions'
+        },
+        path: ""
     end
   end
-end
+
+  namespace :api do 
+    namespace :v1 do 
+      resources :users, only: [:index] do 
+        resources :reservations, only: [:index, :create, :destroy], :path => 'reservations'
+      end
+      resources :motorcycles, only: [:index, :show, :create, :update]
+      get 'all_motorcycles/', to: 'motorcycles#all_motorcycles', as: 'all_motorcycle'
+      patch 'motorcycle/:id/availability', to: 'motorcycles#availability', as: 'Motorcycle_availability'
+    end
+  end
 end
